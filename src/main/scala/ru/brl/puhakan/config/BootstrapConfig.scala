71f78c74d61ffc783ff09e3501bf28395f40121c
Бесчастных Роман Львович <rbeschastnykh@cinimex.ru>
2026-03-10 23:36:17 +0300

    Keep building config processing

--- src/main/scala/ru/brl/puhakan/config/BootstrapConfig.scala
+++ src/main/scala/ru/brl/puhakan/config/BootstrapConfig.scala
@@ -1,12 +1,12 @@
 package ru.brl.puhakan
 package config
 
-import java.io.{BufferedReader, File, FileInputStream, FileReader, InputStream, Reader}
 import java.net.URI
-import java.nio.file.Path
 import scala.annotation.tailrec
 import scala.io.Source
-import scala.util.{Failure, Success, Try, Using}
+import scala.util.Using
+import scala.collection.BufferedIterator
+import scala.collection.mutable.ArrayBuffer
 
 /** Process config file.
  *
@@ -16,42 +16,56 @@ class BootstrapConfig(private val configFile: URI) {
   loadConfig()
 
   private def loadConfig(): Unit = {
-    val rootNode: ConfNode = ConfNode()
-    Using(Source.fromURI(configFile)) { reader =>
-      val iter: Iterator[String] = reader.getLines().filter(!_.isBlank).iterator
-      val configRootNode: ConfNode = buildTree(ConfNode(), iter, 0)
+    val rootNode: ConfNode = Using(Source.fromURI(configFile)) { reader =>
+      val iter: BufferedIterator[String] = reader.getLines().filter(!_.isBlank).iterator.buffered
+
+      @tailrec
+      def buildTree(confNode: ConfNode): ConfNode = {
+        if (!iter.hasNext) {
+          confNode
+        } else {
+          val currentLine: String = iter.next()
+
+          val colonIndex: Int = currentLine.indexOf(':')
+          require(colonIndex > -1, s"Invalid line $currentLine")
+
+          val (paramName, paramValue): (String, String) = currentLine.splitAt(colonIndex)
+
+          val (value, nextNode): (String, Option[ArrayBuffer[ConfNode]]) = if (!iter.hasNext || compareWhiteSpaces(currentLine, iter.head) <= 0) {
+            require(paramValue != null, s"Invalid line $currentLine")
+            (paramValue.tail.trim, None)
+          } else {
+            (null, Option(ArrayBuffer[ConfNode]()))
+          }
+
+          val node: ConfNode = ConfNode(paramName.trim, value, nextNode)
+
+          confNode._3 match {
+            case Some(ls) => ls += node
+            case None =>
+          }
+
+          node._3 match {
+            case Some(_) => buildTree(node)
+            case None => buildTree(confNode)
+          }
+        }
+
+      }
+
+      buildTree(ConfNode())
     }.toEither match {
       case Left(ex) =>
-        println("Comprehensive logging needed. Exception occured")
+        println("Comprehensive logging needed. Exception occurred")
         throw RuntimeException(ex)
-      case Right(_) =>
+      case Right(va) => va
     }
   }
 
-  @tailrec
-  private def buildTree(confNode: ConfNode, iterator: Iterator[String], previousPad: Int): ConfNode = {
-    val currentLine: String = iterator.next()
-    
-    val colonIndex: Int = currentLine.indexOf(':')
-    require(colonIndex > -1, s"Invalid line $currentLine")
+  private def compareWhiteSpaces(cur: String, next: String): Int = {
+    val currentLineWs: Int = cur.length - cur.dropWhile(c => c == ' ').length
+    val nextLineWs: Int = next.length - next.dropWhile(c => c == ' ').length
 
-    val (paramName, paramValue): (String, String) = currentLine.splitAt(colonIndex)
-    
-    require(paramName != null, s"Invalid line: $currentLine")
-
-    if (!iterator.hasNext) {
-      require(!paramValue.isBlank, s"Invalid line: $currentLine")
-      ConfNode(paramName.trim, paramValue, Array())
-    } else {
-      if (!paramValue.isBlank) {
-        ConfNode(paramName, Array(paramValue.tail.trim))
-        confNode.append()
-      } else {
-        val newNode: ConfNode = ConfNode(paramName, Array())
-        
-      }
-//      buildTree()
-    }
-
+    nextLineWs - currentLineWs
   }
 }
--- src/main/scala/ru/brl/puhakan/config/ConfNode.scala
+++ src/main/scala/ru/brl/puhakan/config/ConfNode.scala
@@ -1,12 +1,14 @@
 package ru.brl.puhakan
 package config
 
+import scala.collection.mutable.ArrayBuffer
+
 /** Node in a yml-config tree.
  *
  * @author Roman Beschastnykh
  * @since 0.0.0
  */
 case class ConfNode(private val name: String = null.asInstanceOf[String],
-                    private val value: Any,                   
-                    private val children: Array[ConfNode] = Array()) {
+                    private val value: String = "",                   
+                    private val children: Option[ArrayBuffer[ConfNode]] = Some(ArrayBuffer())) {
 }
